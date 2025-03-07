c7b2a0b2db4b3e23d29f356389751127e87f6411
Rogier <s3718174@leidenuniv.nl>
2025-03-07 11:13:29 +0100

    small remark

--- visualiser.py
+++ visualiser.py
@@ -15,20 +15,22 @@ class Visualiser:
         # TODO: plotly
         fig = go.Figure()
 
+        # TODO: I think missing values should just be ignored,
+        # so only the remaining values should be used
         # drop columns containing null values
         plot_data = self.fanova_results.dropna(axis=1)
-        
+
         if plot_data.shape[1] == 0:
             print("no columns left after dropping NaN columns")
-            return fig  
+            return fig
 
         hyperparameters = list(plot_data.columns)
 
         for hp in hyperparameters:
-            fig.add_trace(go.Violin(x=[hp]*(plot_data.shape[0]), 
-                                    y=plot_data[hp].tolist(), 
+            fig.add_trace(go.Violin(x=[hp]*(plot_data.shape[0]),
+                                    y=plot_data[hp].tolist(),
                                     name=hp,
-                                    box_visible=True, 
+                                    box_visible=True,
                                     meanline_visible=True))
 
         fig.update_layout(title="Variance Contribution of Hyperparameters",
