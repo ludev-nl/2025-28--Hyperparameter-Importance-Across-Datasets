b58e376db5bcc77a16750a620cbfabdb410a07a9
Rogier <s3718174@leidenuniv.nl>
2025-03-28 13:32:52 +0100

    Fixing configspace; changed categorical params to store and update analogously to numeric in dict; fixing constant hyperparams; using standard dict to configspace conversion

--- fanovaservice.py
+++ fanovaservice.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 
 from fanova import fANOVA
-from ConfigSpace import ConfigurationSpace 
+from ConfigSpace import ConfigurationSpace
 from  ConfigSpace import UniformFloatHyperparameter
 from ConfigSpace import CategoricalHyperparameter
 # TODO: might be unnecessary if we implement configspace
@@ -26,70 +26,50 @@ class FanovaService:
 
         # TODO: no more one hot encoding once configspace is implemented
 
-        cs = ConfigurationSpace()
+        param_dict = {}
+        first = True
 
-        col_temp = {}
-        task_temp = {}
-        unique_temp = []
-        first = 1
-        for task, unfilered_data in self.raw_data.items():
-            data = unfilered_data.drop(columns=['value'], errors='ignore') 
+        for _, raw_data in self.raw_data.items():
+            data = raw_data.drop(columns=['value'])
             num_cols = data.select_dtypes(include=['number']).columns
             cat_cols = data.select_dtypes(exclude=['number']).columns
 
-            #ad numerical hyperparameters
-            print(col_temp)
+            # Min and max of numerical hyperparams
             for col in num_cols:
                 min_val = data[col].min()
                 max_val = data[col].max()
+                if first:
+                    param_dict[col] = (min_val, max_val)
+                else:
+                    prev_min, prev_max = param_dict[col]
+                    param_dict[col] = (min(prev_min, min_val), max(prev_max, max_val))
 
-                if (first == 1):
-                    col_temp[col] = (min_val, max_val)
-                else: 
-                    prev_min, prev_max = col_temp[col]
-                    col_temp[col] = (min(prev_min, min_val), max(prev_max, max_val))
-                # if (first == 1):
-                #     col_temp[col] = (min_val, max_val)
-                #     print(col_temp[col][0])
-                # elif (min_val < col_temp[col][0] and max_val > col_temp[col][1]): 
-                #     col_temp[col] = (min_val, max_val)
-                # elif (min_val < col_temp[col][0] and max_val < col_temp[col][1]):
-                #     col_temp[col] = (min_val, col_temp[col][1])
-                # elif (min_val > col_temp[col][0] and max_val > col_temp[col][1]):
-                #     col_temp[col] = (col_temp[col][0], max_val)
-                # elif (min_val > col_temp[col][0] and max_val < col_temp[col][1]):
-                #     col_temp[col] = (col_temp[col][0], col_temp[col][1])
-            first = 0 
-
-            
-            #add categorical hyperparameters
+            # Unique values of categorical hyperparams
             for col in cat_cols:
-                unique_values = list(data[col].dropna().unique())  #drop NaN values
-                unique_temp = unique_temp + unique_values
-        
-        for col, (min_val, max_val) in col_temp.items():
-            # if min_val == max_val:
-            #     cs.add_hyperparameter(Constant(col, value=min_val))  # Fix for identical min/max
-            # else:
-            if (min_val != max_val):
-                cs.add_hyperparameter(UniformFloatHyperparameter(col, lower=min_val, upper=max_val))
-        # for col in num_cols: 
-        #     cs.add(UniformFloatHyperparameter(col, lower=col_temp[col][0], upper=col_temp[col][1]))
+                unique_values = set(data[col].dropna().unique())
+                if first:
+                    param_dict[col] = unique_values
+                else:
+                    param_dict[col] = set.union(param_dict[col], unique_values)
 
+            first = False
 
-        unique_values = list(set(unique_temp))  #drop NaN values
-        cs.add(CategoricalHyperparameter(col, choices=unique_values))
+        # Find constant parameters, and convert sets to lists
+        for param, range in param_dict.items():
+            if isinstance(range, tuple) and range[0] == range[1]:
+                param_dict[param] = range[0]
+            elif isinstance(range, set):
+                if len(range) == 1:
+                    param_dict[param] = range.pop()
+                else:
+                    param_dict[param] = list(range)
 
-        self.auto_cfg_space = cs 
+        cs = ConfigurationSpace(space=param_dict)
+
+        self.auto_cfg_space = cs
         print(cs)
         return cs
-        # one_hot_data = {}
-        # for (task, data) in self.raw_data.items():
-        #     cat = data.select_dtypes(exclude='number').columns
-        #     one_hot_data[task] = pd.get_dummies(data, columns=cat, dtype=float)
-        # self.raw_data = one_hot_data
 
-        # return self.auto_cfg_space
 
     def impute_data(self):
         # TODO: impute using fillna with some value out of range
