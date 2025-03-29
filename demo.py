7f56b583aada301ba344b63fac37652a1cee5ff5
Rogier <s3718174@leidenuniv.nl>
2025-03-29 16:10:48 +0100

    Updated demo, but doesnt work as some functions need to do all data instead of only one task now

--- demo.py
+++ demo.py
@@ -1,24 +1,29 @@
-from openmlfetcher import OpenMLFetcher
-from fanovaservice import FanovaService
-from visualiser import Visualiser
+import openmlfetcher as omlf
+import fanovaservice as fnvs
+import visualiser as vis
 
 flow_id = 6969
 suite_id = 99
-min_runs = 200
-max_runs = 500
+min_runs = 100
+max_runs = 250
 
-# The task loop is not inside the class methods,
+# The task loop is not inside the functions,
 # because we will probably download one task,
 # send an update to the site, and continue with the next
-omlf = OpenMLFetcher(flow_id, suite_id)
-tasks = omlf.fetch_tasks()
-for task in tasks:
-    omlf.fetch_runs(task, max_runs)
-omlf.export_csv()
+tasks = omlf.fetch_tasks(suite_id)
+data = {}
+for task in tasks[:10]:
+    data[task] = omlf.fetch_runs(flow_id, task, max_runs)
 
-fnvs = FanovaService(omlf.results)
-fnvs.run_fanova(min_runs)
-fnvs.export_csv(flow_id, suite_id)
+# All these steps have to be executed in this order, and
+# only the filtering might happen multiple times
+cfg_space = fnvs.auto_configspace(data)
+# Have the user edit the cfg_space as often as they want,
+# and filter every time with new cfg_space
+filtered_data = fnvs.filter_data(data, cfg_space)
+imputed_data, extended_cfg_space = fnvs.impute_data(filtered_data, cfg_space)
+processed_data = fnvs.prepare_data(imputed_data, extended_cfg_space)
+results = fnvs.run_fanova(processed_data, extended_cfg_space, min_runs)
 
-vis = Visualiser(fnvs.results)
-vis.violinplot(show=True)
+vis.violinplot(results, show=True)
+vis.crit_diff_diagram(results, show=True)
