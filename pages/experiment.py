d509cc701a7a2f754ee705bd71b72a3fc1314ea5
ianbdehaan <ianbertholdhaan@gmail.com>
2025-04-04 13:01:11 +0200

    new features in configuration space

--- pages/experiment.py
+++ pages/experiment.py
@@ -8,7 +8,43 @@ dash.register_page(__name__, path='/experiment')
 
 flow_content = html.Div(html.H1("This the flow tab"))
 
-config_content = html.Div(html.H1("This the config tab"))
+config_content = html.Div([
+                              html.Br(),
+                              dbc.Row([
+                                          dbc.Col(html.Div("Minimal runs:")),
+                                          dbc.Col(
+                                                      dbc.Input(
+                                                        type="number",id="min_runs",value=0
+                                                    )
+                                                )
+                                      ]),
+                              html.Br(),
+                              dbc.Row(html.Center(html.Div("Choose hyperparameter configuration space:"))),
+                              html.Br(),
+                              dbc.Row([
+                                            dbc.Col(html.Div("Categorical:")),
+                                            dbc.Col(html.Div("Numerical:"))
+                                      ]),
+                              dbc.Row([
+                                            dbc.Col(html.Div(
+                                                        dcc.Dropdown(["Hyperp 1", "hyparam","1352 hyp"],id='num_hyperparameter')
+                                                    )),
+                                            dbc.Col(html.Div(
+                                                        dcc.Dropdown(["Hyperp 1", "hyparam","1352 hyp"],id='cat_hyperparameter')
+                                                    ))
+                                      ]),
+                              html.Br(),
+                              dbc.Row([
+                                            dbc.Col(
+                                                        html.Div(dcc.Dropdown(["Random Forest", "Transformer","k-Neighbours"],id='categories')),
+                                                    ),
+                                            dbc.Col(
+                                                        html.Div([
+                                                                     dcc.RangeSlider(0,20,marks=None,id='range')
+                                                                 ]),
+                                                    ),
+                                      ])
+                          ])
 
 layout = dbc.Container(
     [
