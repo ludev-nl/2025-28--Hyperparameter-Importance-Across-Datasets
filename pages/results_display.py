ee09927cc9957670c71f49ffe865737d01b55daa
omoideinmyhead <mavis.fu1998@gmail.com>
2025-04-05 18:33:48 +0200

    added layout for results display and loading page

--- pages/results_display.py
+++ pages/results_display.py
@@ -1,9 +1,33 @@
 import dash
-from dash import html
+from dash import html,dcc
+import dash_bootstrap_components as dbc
+import plotly.graph_objects as go
 
 dash.register_page(__name__, path='/results_display')
 
-layout = html.Div([
-    html.H1('This is our results display page'),
-    html.Div('This is our results display page content.'),
-])
+# currently use two blank figures as placeholder
+violinplot = go.Figure()
+cdplot = go.Figure()
+
+layout = dbc.Container([
+        html.H1('Here are the results:'),
+        dbc.Row([
+                dbc.Col([
+                        html.H5('Violin Plot'),
+                        dcc.Graph(figure=violinplot, id='violin-plot'),
+                ], width=6),
+                dbc.Col([
+                        html.H5('Critical Difference Plot'),
+                        dcc.Graph(figure=cdplot, id='cd-plot'),
+                ], width=6)     
+        ]),
+        html.Div([
+            html.Div('Download the csv files:'),
+                    dbc.Button(
+                    'Export CSV',
+                    color='primary',
+                    id='button',
+                    className='mb-3',
+                )
+        ], className='text-center')
+], fluid=True)
--- pages/results_loading.py
+++ pages/results_loading.py
@@ -1,9 +1,16 @@
 import dash
 from dash import html
+import dash_bootstrap_components as dbc
 
 dash.register_page(__name__, path='/results_loading')
 
-layout = html.Div([
-    html.H1('This is our results loading page'),
-    html.Div('This is our results loading page content.'),
-])
+layout = dbc.Container([
+            dbc.Row([
+                dbc.Col([
+                        html.Div('Fetching OpenML...', className='mb-2'),
+                        dbc.Progress(value=50, id='fetch_openml', animated=False, striped=True, className='mb-5'),
+                        html.Div('Running fANOVA...', className='mb-2'),
+                        dbc.Progress(value=50, id='running_fanova', animated=False, striped=True, className='mb-5'),
+                ], width=6, className='mx-auto')
+            ])
+        ])
