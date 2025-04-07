72cfaa64c2a185bbc4d174a1b9986b6bd93165ae
yvonne <a@b.com>
2025-04-07 16:08:20 +0200

    type specification of function arguments

--- help.py
+++ help.py
@@ -1,11 +1,17 @@
 import pandas as pd
+from typing import Dict
 import io
 
-def dictDFtoJSON(frames):
+
+def dictDFtoJSON(frames: Dict[int, pd.DataFrame]) \
+                -> Dict[str, str]:
     newdict = {key: [item.to_json(index=False)]
                for key, item in frames.items()}
     return newdict
 
-def dictJSONtoDF(JsonDict):
-    newdict = {key : pd.read_json(io.StringIO(item[0])) for key, item in JsonDict.items()}
+
+def dictJSONtoDF(JsonDict: Dict[str, str]) \
+                -> Dict[int, pd.DataFrame]:
+    newdict = {key: pd.read_json(io.StringIO(item[0]))
+               for key, item in JsonDict.items()}
     return newdict
