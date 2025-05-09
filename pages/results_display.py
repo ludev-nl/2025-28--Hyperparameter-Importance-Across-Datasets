e8c9cb3d2be439c1e3e465e01f406ff51c0ecebe
ianbdehaan <ianbertholdhaan@gmail.com>
2025-05-09 13:02:08 +0200

    trying to fix plots

--- pages/results_display.py
+++ pages/results_display.py
@@ -10,13 +10,13 @@ from io import StringIO
 
 dash.register_page(__name__, path='/results_display')
 
-# currently use two blank figures as placeholder
-violinplot = go.Figure()
-cdplot = 'data:image/png;base64,'
+# # currently use two blank figures as placeholder
+# violinplot = go.Figure()
+# cdplot = 'data:image/png;base64,'
 
 @callback(
-    [Output("violin-plot", "figure"),
-    Output("critical-distance-plot", "src")],
+    Output("violin-plot", "figure"),
+    Output("critical-distance-plot", "src"),
     Input("fanova_results", "data"),
 )
 def display_results(fanova_results):
@@ -29,7 +29,7 @@ layout = dbc.Container([
         dbc.Row([
                 dbc.Col([
                         html.H5('Violin Plot'),
-                        dcc.Graph(figure=violinplot, id='violin-plot'),
+                        dcc.Graph(id='violin-plot'),
                 ], width=6),
                 dbc.Col([
                         html.H5('Critical Difference Plot'),
