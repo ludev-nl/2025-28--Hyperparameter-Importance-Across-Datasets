b40773470e1d6ffcf4d871d1c9eb2deabdfed90b
ianbdehaan <ianbertholdhaan@gmail.com>
2025-05-09 12:36:59 +0200

    implementation of critical distance plot closes #12

--- visualiser.py
+++ visualiser.py
@@ -1,5 +1,12 @@
 import pandas as pd
 import plotly.graph_objects as go
+import matplotlib.pyplot as plt
+from matplotlib import rcParams
+from io import BytesIO
+import base64
+import scipy.stats as ss
+import scikit_posthocs as sp
+import numpy as np
 
 
 def violinplot(fanova_results: pd.DataFrame,
@@ -37,9 +44,40 @@ def violinplot(fanova_results: pd.DataFrame,
 
 
 def crit_diff_diagram(fanova_results: pd.DataFrame,
-                      show: bool) -> go.Figure:
+                      show: bool) -> str:
     """Create a critical difference diagram of the data in
     fanova_results, and show the plot iff show == True.
     """
-    # TODO: implement
-    return None
+    fanova_results.index = list(range(len(fanova_results)))
+    dict_data = {val: np.array(list(fanova_results[val])) for val in fanova_results.columns}
+    data2 = (
+      pd.DataFrame(fanova_results)
+      .rename_axis('cv_fold')
+      .melt(
+          var_name='estimator',
+          value_name='score',
+          ignore_index=False,
+      )
+      .reset_index()
+    )
+
+    avg_rank = data2.groupby('cv_fold').score.rank().groupby(data2.estimator).mean()
+    ss.friedmanchisquare(*dict_data.values())
+    test_results = sp.posthoc_conover_friedman(
+        data2,
+        melted=True,
+        block_col='cv_fold',
+        block_id_col='cv_fold',
+        group_col='estimator',
+        y_col='score',
+    )
+    rcParams.update({'figure.autolayout': True})
+    fig,ax = plt.subplots(figsize=[8,4])
+    sp.critical_difference_diagram(avg_rank, test_results,ax=ax)
+    buf = BytesIO()
+    plt.savefig(buf, format='png')
+    fig_data = base64.b64encode(buf.getbuffer()).decode("utf8")
+    buf.close()
+    fig_bar_matplotlib = f'data:image/png;base64,{fig_data}'
+
+    return fig_bar_matplotlib
