538a203a9725ecaceca04d38c4fbee179e6ccaa6
angelavandelft <angelavandelft13@gmail.com>
2025-05-19 16:44:19 +0200

    colors of graphs aligned

--- visualiser.py
+++ visualiser.py
@@ -9,6 +9,18 @@ import scipy.stats as ss
 import scikit_posthocs as sp
 import numpy as np
 
+
+
+def colormap(hyperparameters):
+    number_hp = len(hyperparameters)
+    gist_rainbow = mpl.colormaps['gist_rainbow'].resampled(number_hp) 
+    new_colors = gist_rainbow(np.linspace(0,1,number_hp))
+    hex_colors = [mpl.colors.to_hex(c) for c in new_colors]
+    return hex_colors
+
+
+
+
 # def colormap(hyperparameters):
 #     cmap = mpl.colormaps['gist_rainbow']
 #     colormap = {}
@@ -29,18 +41,21 @@ def violinplot(fanova_results: pd.DataFrame,
         print("no columns left after dropping NaN columns")
         return fig
 
-    hyperparameters = list(plot_data.columns)
-    # color_dict = colormap(hyperparameters)
+    hyperparameters = plot_data.rank(axis=1).mean(axis=0).sort_values().index
+
+    color_dict = colormap(hyperparameters)
     # print(color_dict)
+    i = -1
     for hp in hyperparameters:
+        i = i + 1 
         name = hp[0].upper() + hp[1:].replace('_', ' ')
         fig.add_trace(go.Violin(x=[name]*(plot_data.shape[0]),
                                 y=plot_data[hp].tolist(),
                                 name=name,
                                 box_visible=True,
                                 meanline_visible=True,
-                                spanmode='hard')
-                                # line_color=color_dict[hp])
+                                spanmode='hard',
+                                line_color=color_dict[i])
                                 )
 
     fig.update_layout(yaxis_title="Variance Contribution",
@@ -68,7 +83,7 @@ def crit_diff_diagram(fanova_results: pd.DataFrame) -> str:
       )
       .reset_index()
     )
-    # color_dict = colormap(fanova_results.columns)
+    color_dict = colormap(fanova_results.columns)
     avg_rank = data2.groupby('cv_fold').score.rank().groupby(data2.estimator).mean()
     ss.friedmanchisquare(*dict_data.values())
     test_results = sp.posthoc_conover_friedman(
@@ -82,7 +97,7 @@ def crit_diff_diagram(fanova_results: pd.DataFrame) -> str:
     rcParams.update({'figure.autolayout': True})
     fig = figure.Figure(figsize=(8,2))
     ax = fig.subplots()
-    sp.critical_difference_diagram(avg_rank, test_results,ax=ax)# , color_pallete=color_dict)
+    sp.critical_difference_diagram(avg_rank, test_results,ax=ax,  color_palette=color_dict)
     buf = BytesIO()
     fig.savefig(buf, format='png')
     fig_data = base64.b64encode(buf.getbuffer()).decode("utf8")
